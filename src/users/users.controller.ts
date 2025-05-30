bea366a4f511da8084c7cd2ec02c69b8fdd6d9a5
Stefano <tefanoboschetti@gmail.com>
2025-05-29 20:25:18 -0400

    UseGuards 🔑🔑🔑

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-users.dto';
 import { UpdateUserDto } from './dto/update-users.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 import { ObjectId } from 'mongoose';
 
 @Controller('users')
@@ -13,6 +14,8 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+
+  // @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
