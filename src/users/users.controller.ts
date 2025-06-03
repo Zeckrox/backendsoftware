a6d9211fc5c53b4b8b24b2a37bd0ef739faff068
Stefano <tefanoboschetti@gmail.com>
2025-06-03 02:20:36 -0400

    💀

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,20 +26,20 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: ObjectId) {
-    return this.usersService.findOne(id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: ObjectId) {
+  //   return this.usersService.findOne(id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: ObjectId, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: ObjectId, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(id, updateUserDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: ObjectId) {
-    return this.usersService.remove(id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: ObjectId) {
+  //   return this.usersService.remove(id);
+  // }
 
   @Post('login')
   signIn(@Body() signInDto: SignInDto) {
