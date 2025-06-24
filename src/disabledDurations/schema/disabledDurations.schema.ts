1edd315b3bf397d3f53ed3faf679256172e595ca
Stefano <tefanoboschetti@gmail.com>
2025-06-24 13:57:16 -0400

    Cambio en Schema 🀄

--- src/disabledDurations/schema/disabledDurations.schema.ts
+++ src/disabledDurations/schema/disabledDurations.schema.ts
@@ -3,7 +3,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 @Schema({ timestamps: true }) 
 export class DisabledDuration {
 
-  @Prop({ required: true, unique: true }) 
+  @Prop({ required: true}) 
   date: string;
 
   @Prop({ required: true }) 
